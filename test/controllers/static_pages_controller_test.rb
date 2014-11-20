c13506e158231e9934cc73ee181623ffd29bae6c
Carlos Antolín Lucas <carlos.antolin.lucas@gmail.com>
2014-11-20 00:22:20 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Kelly|Kelson|Kelflor"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Halluda primo"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "K dise"
   end
 
 end
